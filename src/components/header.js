a425f95fca37a67f77c3e0a0f536e2b987978416
KRQuinn <kr.quinn82@gmail.com>
2021-08-06 11:44:36 -0700

    Task 2 complete - 5/11 passing

--- src/components/header.js
+++ src/components/header.js
@@ -49,6 +49,8 @@ const headerAppender = (selector) => {
   // It should create a header using the Header component above, passing arguments of your choosing.
   // It should append the header to the element in the DOM that matches the given selector.
   //
+  const headerEntryPoint = document.querySelector(selector)
+    headerEntryPoint.appendChild(Header('Mos Eisley Gazzette', 'A long time ago...', '123°' ));
 }
 
 export { Header, headerAppender }
